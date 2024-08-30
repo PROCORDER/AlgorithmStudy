ce27cea997239d7c3cacd7214f58364bda758692
choijunwon <apro500@naver.com>
2024-08-30 17:47:24 +0900

    Daily Temp

--- Remove_Duplicate_letters/main.py
+++ Remove_Duplicate_letters/main.py
@@ -1,29 +1,15 @@
-
 class Solution(object):
-    def removeDuplicateLetters(self, s):
+    def dailyTemperatures(self, temperatures):
         """
-        :type s: str
-        :rtype: str
+        :type temperatures: List[int]
+        :rtype: List[int]
         """
-        seen = set()  # 이미 추가된 문자를 추적
-        stack = []    # 결과를 저장할 스택
-        last_occurrence = {c: i for i, c in enumerate(s)}  # 각 문자의 마지막 위치
-
-        for i, c in enumerate(s):
-            if c in seen:
-                continue  # 이미 포함된 문자면 건너뛰기
-
-            # 현재 문자가 스택의 마지막 문자보다 작고,
-            # 마지막 위치가 현재 인덱스보다 큰 경우
-            while stack and c < stack[-1] and last_occurrence[stack[-1]] > i:
-                seen.remove(stack.pop())  # 스택에서 제거하고, seen에서 삭제
-
-            seen.add(c)  # 현재 문자를 seen에 추가
-            stack.append(c)  # 스택에 추가
-
-        return ''.join(stack)
-
-
-
-
-        return solution
+        temperaturesLength=len(temperatures)
+        stack=[]
+        List=[0]*temperaturesLength
+        for i, item in enumerate(temperatures):
+            while stack and temperatures[stack[-1]]<item:
+                last=stack.pop()
+                List[last]=i-last
+            stack.append(i)
+        return List
