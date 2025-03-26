d69823d92f6bab5bc873c361803974eb06432c0f
choijunwon <apro500@naver.com>
2025-03-26 09:29:51 +0900

    Palidrome Linked List

---
+++ Palindrome Linked List.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def isPalindrome(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: bool
+        """
+        q=[]
+        if not head:
+            return True
+
+        while head is not None:
+            q.append(head.val)
+            head=head.next
+
+        while len(q)>1:
+            if q.pop(0)!=q.pop():
+                return False
+
+        return True
+
